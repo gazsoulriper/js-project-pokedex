df93f280d1156e5fa759e794da5473e4032911fd
gazsoulriper <gazsoulriper@gmail.com>
2024-01-03 22:02:55 -0300

    Projeto pokedex finalizado

---
+++ pokedex/assets/js/main.js
@@ -0,0 +1,116 @@
+const pokemonDetail = document.getElementById('pokemonDetail')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+ const maxRecords = 99999
+const limit = 8
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}" onClick="clickPokemon(${pokemon.number})">
+            <span class="number">#${pokemon.number.toString().padStart(3,'0')}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+                     
+            </div>
+
+        </li>
+     
+    `
+}
+
+const clickPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    displayModal(pokemon)
+}
+
+const displayModal = (pokemon) =>{
+   
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+ 
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const htmlString = `
+   
+    <div id="modal">
+      <div id="clickPokemon">
+          <button id="closeBtn" onClick="closeModal()">Close</button>
+            <li class="pokemon ${pokemon.type}">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#${pokemon.id.toString().padStart(3,"0")}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            </div>
+                 <img id="img-pokemon" src="${photo}"alt="${pokemon.name}">
+                
+                 <div id="data">
+                 <h4>Base Stats</h4>
+                 <div id="hability">
+                 <div class="stat-desc">
+                  ${pokemon.stats.map((name_stats) =>`<p class="${type}">${name_stats.stat.name}</p>`).join('')}
+                 </div>
+                 <div class="bar-inner"> ${pokemon.stats.map((base_stats) =>`<p class="${type}">${base_stats.base_stat}</p>`).join('')}</div>
+                 </div>
+                 <div id="stats">
+                     <div class="stat-bar">
+                         <p>Height: ${(pokemon.height/10).toFixed(2)}m</p>
+                         <p>Weight: ${(pokemon.weight/10)}kg</p>
+                       </div>
+                </div>
+             </div>
+           </div>
+        </div>
+     </li>
+          
+            
+             
+                
+            
+    
+    `
+   
+    pokemonDetail.innerHTML = htmlString + pokemonDetail.innerHTML
+}
+
+const closeModal = () =>{
+    const modal = document.getElementById('modal')
+    modal.parentElement.removeChild(modal)
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonDetail.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNextPage = offset + limit
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+         const newLimit = maxRecords - offset
+         loadPokemonItens(offset, newLimit)
+
+         loadMoreButton.parentElement.removeChild(loadMoreButton)
+     } else {
+         loadPokemonItens(offset, limit)
+     }
+})
